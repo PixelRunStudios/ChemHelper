991ea93c1167be8387d522febb712ccf797e9485
Markus Feng <zzz1112222595@hotmail.com>
2014-09-30 14:06:58 -0400

    Added EquationBalancer

---
+++ src/com/github/pixelrunstudios/ChemHelper/EquationBalancer.java
@@ -0,0 +1,12 @@
+package com.github.pixelrunstudios.ChemHelper;
+
+import java.util.Map;
+import java.util.Set;
+
+public class EquationBalancer{
+	public static Pair<Map<Map<String, Integer>, Integer>, Map<Map<String, Integer>, Integer>>
+	balance(Set<Map<String, Integer>> in, Set<Map<String, Integer>> out){
+		//TODO
+		return null;
+	}
+}
