3390e527cf71eecc0682ef1a9cd2adf74dde180d
Markus Feng <zzz1112222595@hotmail.com>
2014-10-01 15:07:53 -0400

    Added comment

--- src/com/github/pixelrunstudios/ChemHelper/EquationBalancer.java
+++ src/com/github/pixelrunstudios/ChemHelper/EquationBalancer.java
@@ -6,6 +6,11 @@ import java.util.Set;
 public class EquationBalancer{
 	public static Pair<Map<Map<String, Integer>, Integer>, Map<Map<String, Integer>, Integer>>
 	balance(Set<Map<String, Integer>> in, Set<Map<String, Integer>> out){
+		/*
+		 * Complexity of thing
+		 *
+		 */
+
 		//TODO
 		return null;
 	}
