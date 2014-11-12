95b5ff70e79f83d8daf77b97e45243cc628b7276
Alex <akam1875@gmail.com>
2014-11-12 16:13:49 -0500

    Made window wider

--- src/com/github/pixelrunstudios/ChemHelper/MainGUI.java
+++ src/com/github/pixelrunstudios/ChemHelper/MainGUI.java
@@ -74,7 +74,7 @@ public class MainGUI extends JFrame{
 	public MainGUI(){
 		setTitle("ChemHelper");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 640);
+		setBounds(100, 100, 840, 640);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
